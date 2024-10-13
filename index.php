b1e3fe5504d9efeac3c6ec7fa92fbd04393fbfe6
Daniel Norlin <daniel.l.norlin@gmail.com>
2024-10-13 21:45:44 +0200

    Site title

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title><?php wp_title('|', true, 'right'); ?></title>
+		<title><?php wp_title('|', true, 'right'); bloginfo( 'name' ) ?></title>
 		
 		<?php wp_head(); ?>
 
